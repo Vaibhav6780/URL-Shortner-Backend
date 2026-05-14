c8281397b3b2a76a985622b06dd26a8d0ce878c0
Vaibhav Sharma <vaibhavsharma6060@gmail.com>
2026-05-14 13:54:03 +0530

    URL Shortner backend -5

--- src/routes/Authroutes.js
+++ src/routes/Authroutes.js
@@ -17,10 +17,10 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({
                 message: "All fields are required"
             });
-        }if(password.length<6) return res.status(400).json({message:"Password should be minimum of 6 characters"})
+        } if (password.length < 6) return res.status(400).json({ message: "Password should be minimum of 6 characters" })
         const existed = await (Register.findOne({ email: email }))
         if (existed) {
-            return res.status(400).json({message:"User already exist, Please Login"});
+            return res.status(400).json({ message: "User already exist, Please Login" });
         }
         const encryptedpass = await bcrypt.hash(password, 10);
         const newuser = await Register.create({
@@ -53,11 +53,14 @@ router.post('/login', async (req, res) => {
         const token = jwt.sign({ id: existed._id }, (process.env.JWT_SECRET), { expiresIn: "1h" });
 
         res.cookie("token", token, {
+
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
-            maxAge: "3600000"//one hour
-        })
+
+            secure: true,
+
+            sameSite: "none"
+
+        });
         return res.status(200).json("Successfully Logged in");
 
 
@@ -70,13 +73,13 @@ router.post('/login', async (req, res) => {
 })
 
 router.get('/logout', (req, res) => {
-   try {
-     res.clearCookie("token");
-    res.status(200).json({message:" loggd out sucessfully"});
-   }catch(error){
+    try {
+        res.clearCookie("token");
+        res.status(200).json({ message: " loggd out sucessfully" });
+    } catch (error) {
 
-    return res.json(500).json({message:"Internal server error occured"});
-   }
+        return res.json(500).json({ message: "Internal server error occured" });
+    }
 })
 
 
@@ -90,28 +93,28 @@ router.get('/me', AuthMiddleware, async (req, res) => {
 
 })
 
-router.get("/check-auth",AuthMiddleware, async(req,res)=>{
-      try{
-         const user = await Register.findById(
+router.get("/check-auth", AuthMiddleware, async (req, res) => {
+    try {
+        const user = await Register.findById(
             req.userId
-         ).select("-password");
-         if(!user){
+        ).select("-password");
+        if (!user) {
             return res.status(404).json({
-               message:"User not found"
+                message: "User not found"
             });
-         }
-         return res.status(200).json({
+        }
+        return res.status(200).json({
             user
-         });
-      }catch(error){
+        });
+    } catch (error) {
 
-         console.log(error);
+        console.log(error);
 
-         return res.status(500).json({
-            message:"Server error"
-         });
+        return res.status(500).json({
+            message: "Server error"
+        });
 
-      }
+    }
 
 });
 module.exports = router
