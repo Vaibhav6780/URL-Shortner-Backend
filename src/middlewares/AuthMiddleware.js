a4cbdf211bc617ee7ef4e04e9069a05cc4adb5f2
Vaibhav Sharma <vaibhavsharma6060@gmail.com>
2026-05-16 14:30:38 +0530

    fixing error in middleware

--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -1,40 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt=require('jsonwebtoken');
 
-const AuthMiddleware = (req,res,next)=>{
-
-    console.log(req.cookies);
-
-    try{
-
-        const token = req.cookies.token;
-
-        if(!token){
-
-            return res.status(401).json({
-                message:"Unauthorized"
-            });
-
-        }
-
-        const decoded = jwt.verify(
-            token,
-            process.env.JWT_SECRET
-        );
-
-        req.userId = decoded.id;
-
-        next();
-
-    }catch(error){
-
-        console.log(error);
-
-        return res.status(500).json({
-            message:"Middleware error"
-        });
-
+const AuthMiddleware=(req,res,next)=>{
+    const token=req.cookies.token;
+    if(!token){
+        
+         return res.status(400).json({message:"U are unauthorised"});
     }
-
+    try{
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        req.userId=decoded.id;
+        next();
+    }catch(error){
+        return res.status(401).json({message:"error occured",error:error});
+    }
 }
 
-module.exports = AuthMiddleware;
+module.exports=AuthMiddleware;
